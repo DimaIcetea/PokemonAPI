b68b7ebc61e9f1dbd03c159b5da0488ac47967bf
hakyg <haky.gamer050@gmail.com>
2024-05-06 15:55:58 +0300

    added DELETE req. for CategoryController

--- Pokemon/Controllers/CategoryController.cs
+++ Pokemon/Controllers/CategoryController.cs
@@ -113,6 +113,25 @@ namespace PokemonReview.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{categoryId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteCategory(int categoryId)
+        {
+            if(!_categoryRepository.CategoryExists(categoryId))
+                return BadRequest(ModelState);
+            var categoryDelete = _categoryRepository.GetCategory(categoryId);
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_categoryRepository.DeleteCategory(categoryDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting category");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
     }
 }
 
--- Pokemon/Interfaces/ICategoryRepository.cs
+++ Pokemon/Interfaces/ICategoryRepository.cs
@@ -10,6 +10,7 @@ namespace PokemonReview.Interfaces
         bool CategoryExists(int categoryId);
         bool CreateCategory(Category category);
         bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
         bool Save();
     }
 }
